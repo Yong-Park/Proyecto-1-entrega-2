1900e88e361a857b9a43c6214da4638041ebd4f5
JorgeCab2711 <60406863+JorgeCab2711@users.noreply.github.com>
2021-03-13 17:55:26 -0600

    Ahora si

--- Quote.java
+++ Quote.java
@@ -1,9 +1,8 @@
 public class Quote {
     public String opQuote (String aString){
         char ch  = aString.charAt(0);
-        char  ch2 = aString.charAt(2);
         String resultado = "Este no es";
-        if (ch == '\'' || ch == 'q' && ch2 == '('){
+        if (ch == '\'' || ch == 'q' ){
             resultado  = aString.toUpperCase();
             resultado = resultado.replace("'", "");
             resultado = resultado.replace("QUOTE", "");
