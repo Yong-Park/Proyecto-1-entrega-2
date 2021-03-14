5a6b04228bb3585327772f7919ce685318b34569
JorgeCab2711 <60406863+JorgeCab2711@users.noreply.github.com>
2021-03-13 18:37:47 -0600

    Empezando los test

--- Quote.java
+++ Quote.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class Quote {
     public String opQuote (String aString){
         char ch  = aString.charAt(0);
@@ -10,7 +14,15 @@ public class Quote {
         else{
             System.out.println("Syntax Error");
         }
-
         return  resultado;
     }
+
+    @Test
+    public void testOpQuote(){
+        assertEquals("( 1 + 3 )", opQuote("quote ( 1 + 3 )"));
+        assertEquals("( + 4  5 )", opQuote("' ( + 4  5 )"));
+
+    }
 }
+
+
