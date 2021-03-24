b79bbf5bff977ad47f56c8380f0418c5c838563c
YongBum <59983323+Yong-Park@users.noreply.github.com>
2021-03-23 18:37:30 -0600

    Add files via upload

--- JavaDoc/type-search-index.js
+++ JavaDoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Aritmetrica"},{"p":"<Unnamed>","l":"ATOM"},{"p":"<Unnamed>","l":"Defun"},{"p":"<Unnamed>","l":"Lista"},{"p":"<Unnamed>","l":"Predicado"},{"p":"<Unnamed>","l":"Quote"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Aritmetrica"},{"p":"<Unnamed>","l":"ATOM"},{"p":"<Unnamed>","l":"Defun"},{"p":"<Unnamed>","l":"FuncionesExtra"},{"p":"<Unnamed>","l":"Lista"},{"p":"<Unnamed>","l":"Predicado"},{"p":"<Unnamed>","l":"Quote"}]
